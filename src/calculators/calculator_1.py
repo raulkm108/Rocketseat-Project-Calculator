6ee2f1281ffecae8033a3a41ae373e46173af8d9
raulkm108 <raulkm.civil@gmail.com>
2025-10-16 17:19:39 -0300

    First proccess implemented

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -5,7 +5,7 @@ class Calculator1:
     def calculate(self, request: FlaskRequest) -> Dict: # type: ignore
         body = request.json
         input_data = self.__validate_body(body)
-        print(input_data)
+        splitted_value = input_data/3
         
     def __validate_body(self, body: Dict) -> float: 
         if "number" not in body:
@@ -14,3 +14,7 @@ class Calculator1:
         
         input_data = body["number"]
         return input_data
+    
+    def __first_process(self, splitted_value: float) -> float:
+        new_number = (((splitted_value/4) + 7)**2)*0.257
+        return new_number
