8bef48cef5b430b2b0f8d782a1afe7c54796883d
raulkm108 <raulkm.civil@gmail.com>
2025-10-17 12:37:56 -0300

    New tests
    Added assertiveness of the first test, and implemeting a new test, which tests if the required specific error occurs when the format of the answer is wrong

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,5 +1,6 @@
 from .calculator_1 import Calculator1
 from typing import Dict
+from pytest import raises
 
 class MockRequest:
     def __init__(self, body: Dict ) -> None:
@@ -13,6 +14,24 @@ def test_calculate():
 
     answer = calculator_1.calculate(mock_request)
     
+    #format of the answer
     assert "data" in answer
     assert "Calculator" in answer["data"]
     assert "Result" in answer["data"]
+
+    #Assertiveness of the answer
+    assert answer["data"]["Result"] == 14.25
+    assert answer ["data"]["Calculator"] == 1
+
+def test_calculate_with_body_error():
+    mock_request = MockRequest(body={"something": 1})
+    calculator_1 = Calculator1()
+
+    with raises(Exception) as excinfo:
+        calculator_1.calculate(mock_request)
+
+    assert str(excinfo.value) == "Wrong body format!"
+
+
+
+
