31b3666f17aec5bea102813d7d88a75e26db5c29
raulkm108 <raulkm.civil@gmail.com>
2025-10-17 12:13:59 -0300

    New test file
    Creating a test file to use pytest AND implementing it to tes the calculate method from the Calculator1 class

---
+++ src/calculators/calculator_1_test.py
@@ -0,0 +1,16 @@
+from .calculator_1 import Calculator1
+from typing import Dict
+
+class MockRequest:
+    def __init__(self, body: Dict ) -> None:
+        self.json = body
+
+def test_calculate():
+
+    mock_request = MockRequest(body={"number": 1})
+
+    calculator_1 = Calculator1()
+
+    answer = calculator_1.calculate(mock_request)
+    print()
+    print(answer)
