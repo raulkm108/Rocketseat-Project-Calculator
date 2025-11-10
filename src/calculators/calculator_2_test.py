bdda9eb41e1570da282246b071490d95c6d7b983
raulkm108 <raulkm.civil@gmail.com>
2025-11-10 11:59:27 -0300

    Added the numpyhandler to the test

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,5 +1,6 @@
 from typing import Dict
 from.calculator_2 import Calculator2
+from src.drivers.numpy_handler import NumpyHandler
 
 class MockRequest:
     def __init__(self, body: Dict) -> None:
@@ -8,7 +9,8 @@ class MockRequest:
 def test_calculate():
     mock_request = MockRequest({ "numbers": [2.12, 4.62, 1.32]})
 
-    calculator_2 = Calculator2()
+    driver = NumpyHandler()
+    calculator_2 = Calculator2(driver)
     edited_value = calculator_2.calculate(mock_request)
     
     assert isinstance(edited_value, dict)
