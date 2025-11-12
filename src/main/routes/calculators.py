d173602bd62ade3a2c71f30b3531c406656c82b8
raulkm108 <raulkm.civil@gmail.com>
2025-11-12 10:05:31 -0300

    Changed routes
    Now instead of creating the calculator 1 itself, the factory does it and we import the factory

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,13 +1,12 @@
 from flask import Flask, Blueprint, jsonify, request
-from src.calculators.calculator_1 import Calculator1
-from src.calculators.calculator_2 import Calculator2
-from src.drivers.numpy_handler import NumpyHandler
+from src.main.factories.calculator2_factory import calculator2_factory
+from src.main.factories.calculator1_factory import calculator1_factory
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=['POST'])
 def calculator_1():
-    calc = Calculator1()
+    calc = calculator1_factory()
     final_result = calc.calculate(request)
 
     return jsonify (final_result)
@@ -15,8 +14,7 @@ def calculator_1():
 @calc_route_bp.route("/calculator/2", methods=['POST'])
 def calculator_2():
 
-    numpy_handler = NumpyHandler()
-    calc = Calculator2(numpy_handler)
+    calc = calculator2_factory()
     final_result = calc.calculate(request)
 
     return jsonify (final_result)
