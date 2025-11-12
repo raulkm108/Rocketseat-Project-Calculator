fcda0b6493c9710cb637865c61abd0adb5df9794
raulkm108 <raulkm.civil@gmail.com>
2025-11-12 11:30:16 -0300

    Added 2 methods to the interface

--- src/drivers/interfaces/driver_handler_interface.py
+++ src/drivers/interfaces/driver_handler_interface.py
@@ -6,3 +6,11 @@ class DriverHandlerInterface(ABC):
     @abstractmethod
     def standard_derivation(self, numbers: List[float]) -> float:
         pass
+
+    @abstractmethod
+    def make_variance(self, numbers: List[float]) -> float:
+        pass
+
+    @abstractmethod
+    def make_multiply(self, numbers: List[float]) -> float:
+        pass
