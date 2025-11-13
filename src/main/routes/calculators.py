18f2561b6c18eda7c50935dda1eeaefe4ff3d1f0
raulkm108 <raulkm.civil@gmail.com>
2025-11-13 14:32:14 -0300

    Activating new error_controller
    Added the exception controller to the routes so that it doesnt show a generic error when things go wrong, but instead a custom and more detailed and informative response

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -2,29 +2,41 @@ from flask import Flask, Blueprint, jsonify, request
 from src.main.factories.calculator1_factory import calculator1_factory
 from src.main.factories.calculator2_factory import calculator2_factory
 from src.main.factories.calculator3_factory import calculator3_factory
+from src.errors.error_controller import errors_handler
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=['POST'])
 def calculator_1():
-    calc = calculator1_factory()
-    final_result = calc.calculate(request)
-
-    return jsonify (final_result)
+    try:
+        calc = calculator1_factory()
+        final_result = calc.calculate(request)
+        return jsonify (final_result)
+    except Exception as exception:
+        error_response = errors_handler(exception)
+        return jsonify(error_response["body"]), error_response["status_code"]
 
 @calc_route_bp.route("/calculator/2", methods=['POST'])
 def calculator_2():
 
-    calc = calculator2_factory()
-    final_result = calc.calculate(request)
-
-    return jsonify (final_result)
+    try:
+        calc = calculator2_factory()
+        final_result = calc.calculate(request)
+        return jsonify (final_result)
+    
+    except Exception as exception:
+        error_response = errors_handler(exception)
+        return jsonify(error_response["body"]), error_response["status_code"]
 
 @calc_route_bp.route("/calculator/3", methods=['POST'])
 def calculator_3():
 
-    calc = calculator3_factory()
-    final_result = calc.calculate(request)
+    try:
+        calc = calculator3_factory()
+        final_result = calc.calculate(request)
+        return jsonify (final_result)
 
-    return jsonify (final_result)
+    except Exception as exception:
+        error_response = errors_handler(exception)
+        return jsonify(error_response["body"]), error_response["status_code"]    
 
